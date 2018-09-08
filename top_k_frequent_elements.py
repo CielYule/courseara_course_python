96328f2a135b7fa1c82f7d10710b1d38ca7d9b7c
CielYule <1084094970@qq.com>
2018-09-08 10:37:15 +0800

    Create top_k_frequent_elements.py

---
+++ top_k_frequent_elements.py
@@ -0,0 +1,45 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Given a non-empty array of integers,
+# return the k most frequent elements.
+#
+# For example,
+# Given [1,1,1,2,2,3] and k = 2, return [1,2].
+#
+# Note:
+# You may assume k is always valid,
+# 1 <= k <= number of unique elements.
+# Your algorithm's time complexity must be better
+# than O(n log n), where n is the array's size.
+
+# Bucket Sort Solution
+
+import collections
+
+try:
+    xrange          # Python 2
+except NameError:
+    xrange = range  # Python 3
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counts = collections.Counter(nums)
+        buckets = [[] for _ in xrange(len(nums)+1)]
+        for i, count in counts.items():
+#python3 use 'items' rather than 'iteritems'
+            buckets[count].append(i)
+
+        result = []
+        for i in reversed(xrange(len(buckets))):
+            for j in xrange(len(buckets[i])):
+                result.append(buckets[i][j])
+                if len(result) == k:
+                    return result
+        return result
