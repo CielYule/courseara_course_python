ff479fdf85d6df4d42b9346c3e0fbf2a799cc023
CielYule <1084094970@qq.com>
2018-09-23 21:42:44 +0800

    Create 735_asteroids_collision.py
    To give a line of asteroids' volume， + means -> and - means<-
    Asteroids that have opposite  move will hit, the smaller one will be destroyed, if volume is the same, then both will boom
    Return the last list of asteroids.

---
+++ 735_asteroids_collision.py
@@ -0,0 +1,15 @@
+class Solution:
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        nlist = []
+        for n in asteroids:
+            while nlist and nlist[-1] > 0 and nlist[-1]+ n < 0:
+                nlist.pop()
+            if not nlist or nlist[-1] < 0 or n > 0:
+                nlist.append(n)
+            elif n < 0 and nlist[-1] + n == 0:
+                nlist.pop()
+        return nlist
